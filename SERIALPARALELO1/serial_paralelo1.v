9b564a1befa840ab79b61962a1d17158b747e2c2
Darkgambler <frenzuce@gmail.com>
2021-05-30 11:08:56 -0600

    cascaron de los modulos

---
+++ SERIALPARALELO1/serial_paralelo1.v
@@ -0,0 +1,14 @@
+module serial_paralelo1( output reg [7:0] data_rx000,
+			 output reg valid_rx000, active,
+			 input wire data_out, reset,
+			 input wire clk_4f, clk_32f );
+
+   // ESTRUCTURA DE ENTRADAS Y SALIDAS QUE SE DEBEN USAR
+   // PARA QUE EXISTA COERENCIA
+   
+endmodule // serial_paralelo1
+
+   
+ 
+
+   
---
+++ SERIALPARALELO2/serial_paralelo2.v
@@ -0,0 +1,14 @@
+module serial_paralelo2( output reg IDLE_OUT,
+			 input wire clk_f, clk_4f, clk_32f,
+			 input wire reset, inserter );
+
+ // AL FINAL IDLE_OUT DEBE ESTAR SINCRONIZADO AL FLANCO ALTO clk_f
+
+   
+   
+endmodule // serial_paralelo2
+
+      
+      
+      
+   
